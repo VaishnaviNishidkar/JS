fa57ab01ba742f35d205d128bb5c7d38c7e632c2
https://github.com/VaishnaviNishidkar <vnrajxeog@gmail.com>
2026-04-03 15:42:54 +0530

    Scope in Js

--- Scope_1.js
+++ Scope_1.js
@@ -2,10 +2,14 @@
 function test(){
 
     if(true){
-        var testVar="Inside Block";
-        let testLet="Inside Block";
+        var testVar="Inside Block";  //function scope
+        let testLet="Inside Block";  //block scope
+         console.log(testLet);
     }
     console.log(testVar);
-    console.log(testLet);
+    console.log(testLet); //testLet not defined error aayega kyuki testLet block scope me hai
 
 }
+
+   // console.log(testVar); //testVar not defined error aayega kyuki testVar block scope not accessible outside the function
+test(); 
