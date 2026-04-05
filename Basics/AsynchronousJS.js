735617f3275192d92619a666f6d6a53b45d99733
https://github.com/VaishnaviNishidkar <vnrajxeog@gmail.com>
2026-04-05 19:12:12 +0530

    Asynchronous  nature of JavaScript

---
+++ Basics/AsynchronousJS.js
@@ -0,0 +1,7 @@
+setTimeout(function(){                              //settimeout method is great example of asynchronous js in settimeout function 1st parameter is function whose code will be executed 2nd parameter -> time and ms after which 1st parameter function will get executed
+    console.log("Set timeout executed");
+    
+},4000);  //pause for 4 sec
+
+
+console.log("END of program");
